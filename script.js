f2aaf01ee87ffe5fc221961db5800dcac2a6b65e
Alexis San Javier <alexissj16@knights.ucf.edu>
2023-05-21 07:25:25 -0400

    added files

---
+++ script.js
@@ -0,0 +1,23 @@
+let arr = [
+	{ name: 'sdfdsf', id: 23, nums: [23, 56, 577, 686] },
+	{ name: 'wewef', id: 43, nums: [23, 45, 34, 23] },
+	{ name: '3434', id: 6767, nums: [45, 45, 23, 686] },
+];
+
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+
+var sum = 0;
+for (let i = 0; i < arr.length; i++) {
+	sum = sum + arr[i].nums[0];
+}
+
+// var sum = arr.reduce((index, obj, acc) => {
+// 	acc += obj.nums[0];
+// 	return acc;
+// }, 0);
+
+console.log(sum);
